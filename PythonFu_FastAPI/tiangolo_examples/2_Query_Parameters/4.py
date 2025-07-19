14e92eafdfd664feafcd27215c849dcabadd613d
peliw <peliw@protonmail.com>
2025-07-19 09:04:37 +0330

    example 2_5 updated

--- PythonFu_FastAPI/tiangolo_examples/2_Query_Parameters/4.py
+++ PythonFu_FastAPI/tiangolo_examples/2_Query_Parameters/4.py
@@ -1,18 +1,25 @@
 from fastapi import FastAPI
-
 # Multiple path and query parameters
+# https://fastapi.tiangolo.com/tutorial/query-params/#multiple-path-and-query-parameters
+
+# creating a fastapi app instance as always
 app = FastAPI()
 
+# Function to read user item with optional query parameters
+@app.get("/users/{user_id}/items/{item_id}/")
 
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-    user_id: int, item_id: str, q: str | None = None, short: bool = False
-):
-    item = {"item_id": item_id, "owner_id": user_id}
+# query paramerets here are optional and are defined by default values
+async def read_user_item(user_id:int, item_id: str, q:str | None = None, short:bool = False):
+
+    # first we create a dictionary to hold the item information
+    item = {"item id:" : item_id, "iser_id": user_id}
+
+    # if q is not None (means if its presented in the path), it will be added to the item dictionary.
+    # in second case, if short is True, it will add a message to the item dictionary.
     if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
+        item.update({"query:" : q})
+    if short:
+        item.update("message:" : "This is a short message")
+
+    # finally, we return the item dictionary
     return item
