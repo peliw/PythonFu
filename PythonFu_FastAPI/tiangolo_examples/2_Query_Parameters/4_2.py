a560d1fb8bd97c7eb7f9307ea839a9f9521da619
peliw <peliw@protonmail.com>
2025-07-19 09:12:01 +0330

    4_2 updated

--- PythonFu_FastAPI/tiangolo_examples/2_Query_Parameters/4_2.py
+++ PythonFu_FastAPI/tiangolo_examples/2_Query_Parameters/4_2.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastApi
+
+# by importing typing we want to use Union for type hinting
+from typing import Union
+
 # Multiple path and query parameters
 # https://fastapi.tiangolo.com/tutorial/query-params/#multiple-path-and-query-parameters
 
@@ -7,26 +11,21 @@ app = FastAPI()
 
 # Function to read user item with optional query parameters
 @app.get("/users/{user_id}/items/{item_id}/")
-
-# query paramerets here are optional and are defined by default values
 async def read_user_item(
-    user_id:int,
-    item_id: str, q:str | None = None,
-    short:bool = False
-    ):
-    # also function can be written like this:
-    # async def read_user_item(user_id: int, item_id: str, q: str = None, short: bool = False):
-    # here for better demonstration we use the first one.
-
+    user_id: int, 
+    item_id: str, 
+    q: Union[str, None] = None, # Union is used for optional parameters
+    short: bool = False
+):
     # first we create a dictionary to hold the item information
-    item = {"item id:" : item_id, "iser_id": user_id}
+    item = {"item id": item_id, "user_id": user_id}
 
     # if q is not None (means if its presented in the path), it will be added to the item dictionary.
     # in second case, if short is True, it will add a message to the item dictionary.
     if q:
-        item.update({"query:" : q})
+        item.update({"query": q})
     if short:
-        item.update("message:" : "This is a short message")
+        item.update({"message": "This is a short message"})
 
     # finally, we return the item dictionary
     return item
