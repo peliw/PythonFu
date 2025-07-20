136a6317ac0edb44222b79991758b0a878d499ef
peliw <peliw@protonmail.com>
2025-07-20 10:35:54 +0330

    example api health check completed

---
+++ PythonFu_FastAPI/QUICKTRACK_FastAPI/excercise/1/demos/01 Exercise Files/01_02/app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Inventory management API",
+    description="APIs for managing inventory",
+    version="1.0.0"
+)
+
+@app.get("/health", tags=["HealthCheck"])
+async def health_check():
+    '''health check endpoint to moitor api uptime'''
+    return {
+        "status" : "online",
+        "message" : "inventory management api is running"
+    }
+
+
+
+
+# # for development environment only
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app",
+        host="127.0.0.1", port=8000,
+        reload=True)
